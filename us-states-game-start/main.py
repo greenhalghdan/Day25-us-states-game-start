11aa80b3773ba0265407bf2e5726347aee83fd17
danie <greenhalghdan@gmail.com>
2023-06-09 19:28:15 +0100

    adding list comprehension

--- us-states-game-start/main.py
+++ us-states-game-start/main.py
@@ -14,6 +14,9 @@ listofstates = states["state"].to_list()
 while len(correct_gusses) < 50:
     answer_state = screen.textinput(title=f"{len(correct_gusses)}/50 States Correct", prompt="enter a guess").title()
     if answer_state == "Exit":
+        statestolearn = [state for state in listofstates if state not in correct_gusses]
+        df = pandas.DataFrame(statestolearn, columns=['state'])
+        df.to_csv("states_to_lean.csv")
         break
     if answer_state not in correct_gusses:
         correct_gusses.append(answer_state)
@@ -25,12 +28,11 @@ while len(correct_gusses) < 50:
             location = states[states.state == answer_state]
             d.goto(int(location.x), int(location.y))
             d.write(answer_state)
-statestolearn = []
-with open("states_to_lean.csv", "w") as new_file:
-    for state in listofstates:
-        if state not in correct_gusses:
-            statestolearn.append(state)
+# statestolearn = []
+# with open("states_to_lean.csv", "w") as new_file:
+#     for state in listofstates:
+#         if state not in correct_gusses:
+#             statestolearn.append(state)
 
-df = pandas.DataFrame(statestolearn, columns=['state'])
-df.to_csv("states_to_lean.csv")
+
 
